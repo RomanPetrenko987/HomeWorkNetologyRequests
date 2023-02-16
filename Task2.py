a42dbb0a14da8a3baf326eb834685192507d0792
RomanPetrenko987 <111350544+RomanPetrenko987@users.noreply.github.com>
2023-02-16 15:37:15 +0300

    Create Task2.py

--- Task2.py
+++ Task2.py
@@ -1,29 +1,50 @@
+
 import requests
 import json
+from os import walk
 
 
 
 
-with open(r"C:\test\new2.txt",mode= "rb" ) as file:
-
-    f = file.read()
-
-
-headers = {
-"Content-Type": "application/json",
-"Authorization": "OAuth y0_AgAAAABkjr10AADLWwAAAADYmR_pZCrArxp7StSZ5jX7Rjl16U7RPCQ"
-}
-params = {'path': "new2.txt"}
-#
-file_path = "https://cloud-api.yandex.net/v1/disk/resources/upload/"
-
-
-upload_disk = requests.get(file_path, headers = headers, params=params)
 
 
 
-new_link = upload_disk.json()["href"]
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
 
-put_file = requests.put(new_link, data=f)
-print(put_file)
+
+
+
+
+    def upload(self, file_path: str):
+        """Метод загружает файлы по списку file_list на яндекс диск"""
+        file_list = next(walk("C:\\kot\\кот\\"), (None, None, []))[2]
+        for file_name in file_list:
+            full_path = file_path + file_name
+
+            with open(full_path, mode="rb") as file:
+              open_file = file.read()
+
+            headers = {
+                "Content-Type": "application/json",
+                "Authorization": self.token
+            }
+
+            params = {'path': file_name}
+
+            path = "https://cloud-api.yandex.net/v1/disk/resources/upload/"
+            new_link = requests.get(path, headers=headers, params=params).json()["href"]
+
+            put_file = requests.put(new_link, data=open_file)
+            print(put_file)
+
+
+if __name__ == '__main__':
+    # Получить путь к загружаемому файлу и токен от пользователя
+    path_to_file = "C:\\kot\\кот\\"
+    token = ""
+    uploader = YaUploader(token)
+    result = uploader.upload(path_to_file)
+
 
